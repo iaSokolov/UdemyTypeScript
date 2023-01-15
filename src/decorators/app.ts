91ded6ed106c396583b389b1fe90a650506d622b
Sokolov Ilya <iasokolov@gmail.com>
2023-01-15 23:57:11 +0300

    Декоратор метода

--- src/decorators/app.ts
+++ src/decorators/app.ts
@@ -1,3 +1,4 @@
+// декоратор класса 
 function CreatedAt<T extends { new(...args: any[]): {} }>(constructor: T) {
 
     return class extends constructor {
@@ -11,3 +12,42 @@ class DecoratorClass {
 }
 
 console.log(new DecoratorClass("test"));
+
+// декоратор метода
+function Log() {
+    return (
+        target: Object,
+        key: string | symbol,
+        desc: TypedPropertyDescriptor<(...args: any[]) => any>
+    ): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
+        let oldValue = desc.value; 
+        desc.value = () => {
+            try {
+                console.log('перехват сообщения')
+                
+            } catch (error) {
+                console.log('перехват сообщения')
+            }
+        }
+    }
+}
+
+
+class DecoratorMethod {
+    constructor(public name: String) { }
+
+    /**
+     * method
+     */
+    @Log()
+    public method() {
+        throw Error('ошибка');
+    }
+}
+
+new DecoratorMethod("test").method()
+
+
+
+
+
